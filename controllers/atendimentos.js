2b05c095f26f3957f7a2e6ce93ffdf0f3d1db898
Douglas Emerick <douglas@emerick.net.br>
2021-09-06 14:22:55 -0300

    adicionar atendimento

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,5 +1,12 @@
-module.exports = app => { 
-    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos'))
+const Atendimento = require('../models/atendimentos')
 
-    app.post('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um POST'))
+module.exports = app => { 
+    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'))
+
+    app.post('/atendimentos', (req, res) => {
+        const atendimento = req.body
+
+        Atendimento.adiciona(atendimento)
+        res.send('Post atendimento')
+    })
 }
